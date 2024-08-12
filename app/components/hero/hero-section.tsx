b3368028da8617cdb048589f0292c0a69740d212
Ebalin1411 <ebalinrosecse2006@gmail.com>
2024-08-12 17:00:38 +0530

    hero section code added

---
+++ app/components/hero/hero-section.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <div className="w-full h-screen  bg-gradient-to-b from-black  via-black  to-[#B5B5B5] to-99%">
+      <div className="w-full h-[100px]"></div>
+      <div>
+        <p></p>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import OurTeamSection from "./components/our-team/our-team-section";
+import HeroSection from "./components/hero/hero-section";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
+      <HeroSection />
       <OurTeamSection />
     </main>
   );
