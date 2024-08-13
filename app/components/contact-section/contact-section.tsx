773ad11a6030971144420290fe2af398e5383b1e
Arunesh-M <arunesh123.m@gmail.com>
2024-08-13 13:07:39 +0530

    error validation added

--- app/components/contact-section/contact-section.tsx
+++ app/components/contact-section/contact-section.tsx
@@ -6,10 +6,10 @@ import "../../globals.css";
 
 import { useForm, ValidationError } from "@formspree/react";
 import { environment } from "@/app/environments/environment";
+import { Icon } from "@iconify/react/dist/iconify.js";
 
 const ContactSection = () => {
   const [state, handleSubmit] = useForm(environment.FORMSPREE_PROJECT_ID);
-
   const [input, setInput] = React.useState({
     name: "",
     email: "",
@@ -30,30 +30,41 @@ const ContactSection = () => {
           CodeSirpi
         </div>
       </div>
-      <div className="my-8 text-center text-5xl text-grey-800">
+      <div className="my-8 text-center text-2xl  text-grey-800 sm:text-3xl lg:text-5xl">
         Let&#39;s talk about your project
       </div>
       <div className="w-full flex flex-col items-center justify-center">
         <div className="w-full max-w-[670px] flex gap-x-5 px-4">
-          <input
-            className=" placeholder:text-grey-600 bg-input-box rounded-[28px] h-14 w-[100%] px-5"
-            placeholder="Your Name"
-            value={input.name}
-            onChange={(e) => {
-              setInput({ ...input, name: e.target.value });
-            }}
-          />
-          <input
-            id="email"
-            className="placeholder:text-grey-600 bg-input-box rounded-[28px] h-14 w-[100%] px-5"
-            placeholder="Your Email Address"
-            value={input.email}
-            onChange={(e) => {
-              setInput({ ...input, email: e.target.value });
-            }}
-          />
-          <ValidationError prefix="Email" field="email" errors={state.errors} />
+          <div className="w-[100%]">
+            <input
+              className=" placeholder:text-grey-600 bg-input-box rounded-[28px] h-14 w-full px-5"
+              placeholder="Your Name"
+              value={input.name}
+              onChange={(e) => {
+                setInput({ ...input, name: e.target.value });
+              }}
+            />
+          </div>
+
+          <div className="w-[100%]">
+            <input
+              id="email"
+              className="placeholder:text-grey-600 bg-input-box rounded-[28px] h-14 w-full px-5"
+              placeholder="Your Email Address"
+              value={input.email}
+              onChange={(e) => {
+                setInput({ ...input, email: e.target.value });
+              }}
+            />
+            {state.errors && (
+              <div className="flex items-center">
+                <Icon icon="codicon:error" color="#f87171" />
+                <p className="text-red-400 m-2">Invalid email</p>
+              </div>
+            )}
+          </div>
         </div>
+
         <div className="w-full max-w-[670px] px-4">
           <textarea
             id="message"
@@ -72,12 +83,18 @@ const ContactSection = () => {
               ...input,
             })
           }
-          className="bg-[#222222] py-2 px-3.5 mt-10"
+          className="bg-[#222222] py-2 px-3.5 mt-10 mb-5"
         >
           Submit
         </button>
+        {state.succeeded && (
+          <div className="flex gap-x-2 items-center">
+            <Icon icon="teenyicons:tick-circle-solid" color="#4ADE80" />
+            <p className=" text-lg text-green-400">Thanks for Submitting!</p>
+          </div>
+        )}
       </div>
-      <div className="text-xl font-medium text-center mt-20 mb-12 text-grey-400 ">
+      <div className="text-xl font-medium text-center mt-14 mb-12 text-grey-400 ">
         2024 CodeSirpi Software LLP
       </div>
     </div>
