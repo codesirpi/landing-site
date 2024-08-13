26f0e26accecdcdf22fc95c1df81ad754a939b47
kns786 <doolocompany@gmail.com>
2024-08-13 12:04:35 +0530

    feat: reading from env file

--- app/components/contact-section/contact-section.tsx
+++ app/components/contact-section/contact-section.tsx
@@ -4,10 +4,11 @@ import logo from "../../assets/contact-section/logo.png";
 import Image from "next/image";
 import "../../globals.css";
 import { useForm, ValidationError } from '@formspree/react';
+import { environment } from "@/app/environments/environment";
 
 
 const contactSection = () => {
-  const [state, handleSubmit] = useForm("xrbzkrzn");
+  const [state, handleSubmit] = useForm(environment.FORMSPREE_PROJECT_ID);
 
   const [input, setInput] = React.useState({
     name: '',
@@ -61,6 +62,7 @@ const contactSection = () => {
           className="placeholder:text-grey-600 bg-input-box border-0 rounded-[28px] max-w-[670px] h-32 w-[100%] px-5 py-2 mt-16"
           id="message"
           placeholder="Please tell us a brief about your project"
+          value={input.message}
           onChange={(e) => {
             setInput({ ...input, message: e.target.value });
           }}
@@ -71,9 +73,12 @@ const contactSection = () => {
             errors={state.errors}
         />
 
-        <button disabled={state.submitting} onClick={ () => handleSubmit({
-          ...input
-        })} className="bg-[#222222] py-2 px-3.5 mt-10">Submit</button>
+        <button disabled={state.submitting} onClick={ () => {
+          handleSubmit({
+            ...input
+          });
+
+        }} className="bg-[#222222] py-2 px-3.5 mt-10">Submit</button>
       </div>
       <div className="text-xl font-medium text-center mt-20 mb-12 text-grey-400 ">
         2024 CodeSirpi Software LLP
