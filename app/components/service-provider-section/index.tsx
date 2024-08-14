2de9991ba0b3f4d53c707b12cc7c2d02fd9bfb25
ajithkumar-kasi <ajithkumarkasi555@gmail.com>
2024-08-14 16:38:48 +0530

    fixed responsive in provider section

--- app/components/service-provider-section/index.tsx
+++ app/components/service-provider-section/index.tsx
@@ -6,14 +6,14 @@ import WebDesignSVG from "../../assets/service-provider-sections/web-design.svg"
 import MVPBuilderSVG from "../../assets/service-provider-sections/mvp-builder.svg";
 import DataVisualizationSVG from "../../assets/service-provider-sections/data-visualization.svg";
 import WaveImage from "../../assets/wave.svg";
-import { motion } from 'framer-motion';
-
+import { motion } from "framer-motion";
 
 const services = [
   {
     title: "Web Design",
     imge: WebDesignSVG,
-    description: "Enhance your digital presence quickly with high quality website",
+    description:
+      "Enhance your digital presence quickly with high quality website",
   },
   {
     title: "MVP Builder",
@@ -23,29 +23,31 @@ const services = [
   {
     title: "Data Visualization",
     imge: DataVisualizationSVG,
-    description: "Turn data into engaging visuals to track trends & gain insights",
+    description:
+      "Turn data into engaging visuals to track trends & gain insights",
   },
 ];
 
 export const ServiceProviderSection = () => {
   return (
-    <div className="flex flex-col w-full pt-20 bg-white">
-      <motion.div 
-      initial={{
-        y: 5,
-        opacity: 0,
-    }}
-    whileInView={{
-        y:0,
-        opacity: 1,
-    }}
-    transition={{
-        duration: 0.7,
-    }}
-    viewport={{
-        once: true
-    }}
-      className="flex flex-col bg-white w-full items-center justify-center px-2">
+    <div className="flex flex-col w-full h-full relative py-20 bg-white">
+      <motion.div
+        initial={{
+          y: 5,
+          opacity: 0,
+        }}
+        whileInView={{
+          y: 0,
+          opacity: 1,
+        }}
+        transition={{
+          duration: 0.7,
+        }}
+        viewport={{
+          once: true,
+        }}
+        className="flex flex-col bg-white w-full items-center justify-center px-10"
+      >
         <div className="my-6 text-violet-700 font-medium text-xl ">
           What we do?
         </div>
@@ -55,7 +57,7 @@ export const ServiceProviderSection = () => {
         </div>
 
         {/* show Projects provider component */}
-        <div className="grid grid-cols-1 items-center my-8  gap-10">
+        <div className="grid grid-cols-1 items-center py-8  gap-10 ">
           {services.map(({ title, imge, description }, index: number) => (
             <ServiceContainer
               key={title}
@@ -66,11 +68,8 @@ export const ServiceProviderSection = () => {
             />
           ))}
         </div>
-    </motion.div>
-
-    <div className="flex w-full bg-blue-wave h-20 bg-cover bg-white">
-        {/* <Image src={WaveImage} alt="wave" /> */}
-      </div>
+      </motion.div>
+      <div className=" bg-blue-wave w-full absolute bottom-0 bg-cover lg:bg-cover bg-no-repeat h-10 md:h-20"></div>
     </div>
   );
 };
--- app/components/service-provider-section/services-container.tsx
+++ app/components/service-provider-section/services-container.tsx
@@ -2,8 +2,7 @@ import React from "react";
 import Image, { StaticImageData } from "next/image";
 import clsx from "clsx";
 import WebDesign from "../../assets/service-provider-sections/web-design.svg";
-import { motion, useInView } from 'framer-motion';
-
+import { motion, useInView } from "framer-motion";
 
 type ServiceContainerProps = {
   title: string;
@@ -20,69 +19,43 @@ export const ServiceContainer = ({
 }: ServiceContainerProps) => {
   return (
     <motion.div
-    initial={{
+      initial={{
         opacity: 0,
         x: isCardIsEven ? 100 : -100,
-        y: isCardIsEven ? 20: -20,
-    }}
-    whileInView={{
+        y: isCardIsEven ? 20 : -20,
+      }}
+      whileInView={{
         opacity: 1,
         x: 0,
-        y:0,
+        y: 0,
         transition: {
-          duration: 2
-        }
-    }}
-    viewport={{ once: true }}
-
-    className={clsx("flex flex-col", {
-        "lg:flex-row-reverse": isCardIsEven,
-        "lg:flex-row": !isCardIsEven,
+          duration: 2,
+        },
+      }}
+      viewport={{ once: true }}
+      className={clsx("flex flex-col ", {
+        "lg:flex-row-reverse gap-10 lg:gap-0": isCardIsEven,
+        "lg:flex-row gap-10": !isCardIsEven,
       })}
     >
       {/* image section*/}
-      <div className="flex overflow-hidden w-[50%]">
-        <Image src={image} alt={title} className="flex object-none" />
+      <div className="flex overflow-hidden lg:w-[50%] md:justify-center">
+        <Image src={image} alt={title} className=" object-cover" />
       </div>
       {/*description and title section*/}
-      <div className={"flex flex-col w-[50%] justify-center items-center lg:items-start text-center lg:text-start text-grey-500 "}>
+      <div
+        className={clsx(
+          "flex flex-col lg:w-[50%] justify-center items-center lg:items-start text-center lg:text-start text-grey-500 ",
+          { "lg:ml-10": isCardIsEven, "lg:ml-0": !isCardIsEven }
+        )}
+      >
         <h5 className="text-xl lg:text-4xl font-bold text-black py-4">
           {title}
         </h5>
-        <div className="flex  w-full text-center lg:text-start md:w-[60%] lg:w-[60%] font-medium text-xl">
-         {description}
+        <div className="flex  w-full text-center lg:text-start md:w-[60%] lg:w-[70%] font-medium text-xl">
+          {description}
         </div>
       </div>
     </motion.div>
   );
 };
-
-
-// export const ServiceContainer = ({
-//     image,
-//     title,
-//     description,
-//     isCardIsEven,
-//   }: ServiceContainerProps) => {
-//     return (
-//       <div className={clsx("flex flex-row justify-between items-center", {
-//         "flex-row-reverse": isCardIsEven  
-//       })}>
-//         {/* image section*/}
-//         <div className="flex object-cover">
-//           <Image src={image} alt={title} className="flex fit" />
-//         </div>
-
-//         {/* title and description section */}
-//         <div className="flex flex-col ml-10">
-//         <h5 className="text-xl lg:text-4xl font-bold text-black py-4">
-//           {title}
-//          </h5>
-//         <div className="flex  w-full text-center lg:text-start md:w-[60%] lg:w-[60%] font-medium text-xl">
-//           {description}
-//         </div>
-//       </div>
-//       </div>
-//     )
-//   }
-  
